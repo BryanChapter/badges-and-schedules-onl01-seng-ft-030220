2f9dc1d4bc4f423aca01ced9c1a52d9f98eeb908
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-06 23:14:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each{|name| new_array << "Hello, my name is #{name}."}
+  new_array
+  end
+end
