df9c5750e63742feb95efe924ce1a42a9262d843
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-06 23:12:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,5 @@
 name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker
-  badge_maker.each do |name|
-  puts "Hello, my name is #{name}."
-  end
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end
