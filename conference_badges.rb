b104b9ed17bf447a9e46e8f1e47862a34320b441
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-06 23:14:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,13 @@ def batch_badge_creator(array)
   new_array
   end
 end
+
+def assign_rooms(speakers)
+   array = []
+    room = 1
+    speakers.each do |name|
+      array.push("Hello, #{name}! You'll be assigned to room #{room}!")
+    room += 1
+  end
+    array
+end
