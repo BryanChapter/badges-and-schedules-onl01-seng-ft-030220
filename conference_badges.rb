9264810dc60c419afbe311ac3d03c54865761ce5
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-06 23:15:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,3 +18,12 @@ def assign_rooms(speakers)
   end
     array
 end
+
+def printer(attendees)
+   batch_badge_creator(attendees).each do |badge|
+     puts badge
+ end
+   assign_rooms(attendees).each do |array|
+     puts array
+   end
+ end
